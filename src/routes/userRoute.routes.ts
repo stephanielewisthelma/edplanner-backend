3a4bda876d0c1af1ce79e8b5a0f465c45d99169d
Uchenna99 <uchennaagbu@gmail.com>
2025-09-18 10:49:18 +0100

    added middleware to routes

--- src/routes/userRoute.routes.ts
+++ src/routes/userRoute.routes.ts
@@ -1,13 +1,14 @@
 import express from "express"
 import { UserController } from "../controllers/userController.control";
+import { authenticateUser } from "../middlewares/auth.middleware";
 
 
 const userController = new UserController();
 const userRouter = express.Router();
 
-userRouter.get("/allusers", userController.getAllUsers);
-userRouter.get("/getuser/:id", userController.getUserbyId);
-userRouter.put("/updateuser/:id", userController.updateUser)
-userRouter.delete("/deleteuser/:id", userController.deleteUser)
+userRouter.get("/allusers", authenticateUser, userController.getAllUsers);
+userRouter.get("/getuser/:id", authenticateUser, userController.getUserbyId);
+userRouter.put("/updateuser/:id", authenticateUser, userController.updateUser);
+userRouter.delete("/deleteuser/:id", authenticateUser, userController.deleteUser);
 
 export default userRouter;
